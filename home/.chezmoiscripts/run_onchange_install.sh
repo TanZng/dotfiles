5251f59dd2b2fad612b47f8abb6ef6674df467aa
tets <wdw>
2022-06-23 10:58:52 -0500

    add bat and nvim config scripts

--- home/.chezmoiscripts/run_onchange_install.sh
+++ home/.chezmoiscripts/run_onchange_install.sh
@@ -4,9 +4,8 @@ arch_install()
 {
     sudo pacman -Syu
     sudo pacman -S --needed makepkg git base-devel \
-    kitty bat lsd zsh ripgrep \
-    zsh-syntax-highlighting zsh-autosuggestions \
-    fzf
+    kitty bat lsd zsh ripgrep fzf \
+    zsh-syntax-highlighting zsh-autosuggestions
 
     # Install if not root profile
     if [ "$EUID" -ne 0 ]
@@ -22,7 +21,8 @@ arch_install()
     fi
 }
 
-install_yay(){
+install_yay()
+{
     mkdir /tmp/yay
     cd /tmp/yay
     curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
@@ -30,6 +30,24 @@ install_yay(){
     cd -
     rm -rf /tmp/yay
     yay --version
+}
+
+debian_install()
+{
+    sudo apt update
+    sudo apt upgrade
+    sudo apt install wget git zsh ripgrep fzf kitty \
+    zsh-syntax-highlighting zsh-autosuggestions
+
+    # install lsd
+    wget https://github.com/Peltoche/lsd/releases/download/0.22.0/lsd_0.22.0_amd64.deb
+    sudo dpkg -i lsd_0.22.0_amd64.deb
+    rm lsd_0.22.0_amd64.deb
+
+    # install bat
+    wget https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb
+    sudo dpkg -i bat_0.21.0_amd64.deb
+    rm bat_0.21.0_amd64.deb
 }
 
 ###
@@ -42,13 +60,12 @@ if [ -f "/etc/os-release" ]; then
     ID_LIKE=$ID_LIKE
 fi
 
-if [ $ID == "arch" ]
-then
+if [ "$ID" = "arch" ]; then
    echo "This is Arch!"
    arch_install
-elif [ $ID_LIKE == "debian" ] || [ $ID == "ubuntu" ]
-then
+elif [ "$ID_LIKE" = "debian" ] || [ "$ID" = "ubuntu" ]; then
    echo "This is Debian!"
+   debian_install
 else
    echo "No arch or debian"
 fi
